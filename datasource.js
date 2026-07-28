bcc952d94e80cd7549419ce640dba2f9c7128e1e
Siddharth Iyer <siyer2405@gmail.com>
2026-07-27 23:34:52 -0400

    Postgres swap

--- datasource.js
+++ datasource.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "courtclash.sqlite",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
   logging: false,
 });
 
